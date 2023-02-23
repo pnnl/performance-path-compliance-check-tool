08c55d412c1615e49f2ce71640dbfc9a46866f53
Gonzalez, Juan <juan.gonzalez@pnnl.gov>
2023-02-23 10:12:49 -0500

    Updated output CSV to include calculated values for FAILED rules.

--- src/process_output_json.py
+++ src/process_output_json.py
@@ -13,7 +13,7 @@ output_csv_path = f"C:\\git\\ruleset-checking-tool\\examples\\output\{csv_name}.
 output_dict = json.load(output_json)
 
 
-def parse_result_dictionary(output, n_passed, n_failed, n_undetermined, message):
+def parse_result_dictionary(output, n_passed = 0, n_failed = 0, n_undetermined = 0, message = None, calc_vals = None):
 
     for result_dict in output["result"]:
 
@@ -23,6 +23,8 @@ def parse_result_dictionary(output, n_passed, n_failed, n_undetermined, message)
                 n_passed += 1
             elif output["result"] == "FAILED":
                 n_failed += 1
+                if calc_vals is None and "calc_vals" in result_dict:
+                    calc_vals = result_dict["calc_vals"]
             elif output["result"] == "UNDETERMINED":
                 n_undetermined += 1
                 if message is None:
@@ -33,7 +35,7 @@ def parse_result_dictionary(output, n_passed, n_failed, n_undetermined, message)
         # If results are a list rather than flat dictionary, recursively search each result and update counts
         elif isinstance(result_dict["result"], list):
 
-            n_passed, n_failed, n_undetermined, message = parse_result_dictionary(result_dict, n_passed, n_failed, n_undetermined, message)
+            n_passed, n_failed, n_undetermined, message, calc_vals = parse_result_dictionary(result_dict, n_passed, n_failed, n_undetermined, message, calc_vals)
 
         else:
 
@@ -41,12 +43,14 @@ def parse_result_dictionary(output, n_passed, n_failed, n_undetermined, message)
                 n_passed += 1
             elif result_dict["result"] == "FAILED":
                 n_failed += 1
+                if calc_vals is None and "calc_vals" in result_dict:
+                    calc_vals = result_dict["calc_vals"]
             elif result_dict["result"] == "UNDETERMINED":
                 n_undetermined += 1
                 if message is None:
                     message = result_dict["message"]
 
-    return n_passed, n_failed, n_undetermined, message
+    return n_passed, n_failed, n_undetermined, message, calc_vals
 
 
 with open(output_csv_path, 'w', newline='') as file:
@@ -54,19 +58,14 @@ with open(output_csv_path, 'w', newline='') as file:
     writer = csv.writer(file)
 
     # Write headers
-    writer.writerow(["Rule ID", "Ruleset Reference", "Rule Description", "Primary Rule", "Outcome", "N_Pass", "N_Fail", "N_Undetermined", "Message"])
+    writer.writerow(["Rule ID", "Ruleset Reference", "Rule Description", "Primary Rule", "Outcome", "N_Pass", "N_Fail", "N_Undetermined", "Message", "Calculated_Values"])
 
     for output in output_dict:
 
         rule_id = output["id"]
-        n_passed = 0
-        n_failed = 0
-        n_undetermined = 0
-        n_not_applicable = 0
-        message = None
 
         # Parse and update results
-        n_passed, n_failed, n_undetermined, message = parse_result_dictionary(output, n_passed, n_failed, n_undetermined, message)
+        n_passed, n_failed, n_undetermined, message, calc_vals = parse_result_dictionary(output)
 
         writer.writerow([f"'{rule_id}",
                          output["standard_section"],
@@ -76,4 +75,5 @@ with open(output_csv_path, 'w', newline='') as file:
                          n_passed,
                          n_failed,
                          n_undetermined,
-                         message])
+                         message,
+                         calc_vals])
